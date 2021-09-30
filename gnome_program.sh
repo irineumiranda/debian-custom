0f757ae53e4fb0c98b19521a375549ba27b0fccf
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2021-09-30 14:26:29 -0300

    instalar_programas

---
+++ gnome_program.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt install vlc vlc-l10n libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-l10n-pt-br gnome-screenshot transmission filezilla audacity gimp inkscape obs-studio
