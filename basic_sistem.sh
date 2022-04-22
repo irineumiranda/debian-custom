0e8512c9f1ba3e04d01b7187a69e6c5cea6a1764
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-04-22 19:25:17 -0300

    Update basic_sistem.sh

--- basic_sistem.sh
+++ basic_sistem.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt install neofetch vim nvidia-detect dkms ffmpeg file-roller zip unzip p7zip-full rar unrar htop -y
+sudo apt install neofetch vim nvidia-detect dkms ffmpeg git wget file-roller zip unzip p7zip-full rar unrar htop -y
