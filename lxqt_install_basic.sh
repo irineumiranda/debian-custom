c853d4d1e9bc4f5ebb5947dbb55162ddad04ab0b
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-04-22 19:39:46 -0300

    Create lxqt_install_basic.sh

---
+++ lxqt_install_basic.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash 
+
+sudo apt install lximage-qt xfwm4 lxqt-about lxqt-admin qterminal lxqt-branding-debian pcmanfm-qt pcmanfm-qt-l10n lxqt-openssh-askpass lxqt-powermanagement lxqt-sudo pavucontrol pavucontrol-qt qlipper qps qttranslations5-l10n lightdm xarchiver
