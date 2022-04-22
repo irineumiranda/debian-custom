6ae3beedde93a750e7238350fcce0178a595ae23
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-04-21 21:23:17 -0300

    Update gnome_program.sh

--- gnome_program.sh
+++ gnome_program.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt install vlc vlc-l10n libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-l10n-pt-br gnome-screenshot transmission filezilla audacity gimp inkscape obs-studio
+sudo apt install vlc vlc-l10n libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-l10n-pt-br gnome-screenshot transmission audacity gimp inkscape obs-studio
