875cebdfe198f0aca2dcba0ac56bf7f357d23481
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-04-21 22:13:38 -0300

    Create xfce4_install_basic.sh

---
+++ xfce4_install_basic.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash 
+
+sudo apt install xorg xfce4 thunar thunar-volman xfce4-power-manager xfce4-session xfce4-indicator-plugin xfce4-power-manager xfce4-battery-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-wavelan-plugin xfce4-screenshooter xfce4-sensors-plugin xfce4-appfinder xfwm4 xfce4-timer-plugin xfce4-whiskermenu-plugin 
