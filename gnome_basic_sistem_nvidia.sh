4c12caa419ce1e91277d75be508c77d2a1043290
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-04-21 21:12:39 -0300

    Update gnome_basic_sistem_nvidia.sh

--- gnome_basic_sistem_nvidia.sh
+++ gnome_basic_sistem_nvidia.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt install neofetch vim nvidia-driver dkms file-roller zip unzip p7zip-full rar unrar htop -y
+sudo apt install neofetch vim nvidia-detect dkms file-roller zip unzip p7zip-full rar unrar htop -y
