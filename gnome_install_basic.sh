84ab54b8630523f294f450bef39d2061b2e85079
Texugo_japa <89923904+irineumiranda@users.noreply.github.com>
2022-06-29 23:24:16 -0300

    Update gnome_install_basic.sh

--- gnome_install_basic.sh
+++ gnome_install_basic.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo apt install --no-install-recommends xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-vesa gnome-session gnome-shell gnome-keyring libpam-gnome-keyring gnome-control-center network-manager-gnome gnome-tweak gnome-tweak-tool gnome-terminal gdm3 gnome-screenshot nautilus bash-completion build-essential xdg-utils
+sudo apt install --no-install-recommends xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-vesa gnome-session gnome-shell gnome-keyring libpam-gnome-keyring gnome-control-center network-manager-gnome gnome-tweak gnome-tweak-tool gnome-terminal gdm3 gnome-screenshot gnome-shell-extension-dashtodock nautilus bash-completion build-essential xdg-utils
